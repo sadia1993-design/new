aec67e450e003a6687c0382d320de166f8428089
Nishat Sanjida <56416902+sadia1993-design@users.noreply.github.com>
2023-03-25 03:05:47 +0600

    bootstrap

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import '@/styles/globals.css'
+import React from 'react';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <React.StrictMode>
+        <Component {...pageProps} />
+      </React.StrictMode>
+
+    </>
+  );
 }
