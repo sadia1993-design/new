53038c8bb65551925471fec88b741d68effa2548
Nishat Sanjida <56416902+sadia1993-design@users.noreply.github.com>
2023-03-25 03:42:38 +0600

    .

--- components/About.jsx
+++ components/About.jsx
@@ -1,6 +1,6 @@
 import React,{memo} from "react";
 import Image from "next/image";
-import styles from '@styles/Home.module.css'
+import styles from '@/styles/Home.module.css'
 
 function About() {
   
