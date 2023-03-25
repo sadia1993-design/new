c9b935a68a47c91c90b73686e84c7e533a087bf2
Nishat Sanjida <sanjidan1993@gmail.com>
2023-03-25 11:04:28 +0600

    home 991

--- components/Features.jsx
+++ components/Features.jsx
@@ -8,28 +8,28 @@ function Features() {
         <div className={styles.featureAll}>
         <div className="row">
             <div className="col-lg-3 col-6">
-                <div className="text-center">
-                    <p className='featureIcon' style={{fontWeight:'600', fontSize: '57px', lineHeight: '30px', color: '#4743c9' , marginBottom: '24px'}}>50+</p>
-                    <p className='featureText' style={{fontWeight:'400', fontSize: '18px', lineHeight: '30px', color: '#0c266c' }}>Supported Currencies</p>
+                <div className={`text-center ${styles.featureBox}`}>
+                    <p className={styles.featureIcon} >50+</p>
+                    <p className={styles.featureText} >Supported Currencies</p>
                 </div>
 
             </div>
             <div className="col-lg-3 col-6 ">
-            <div className="text-center">
-                    <p className='featureIcon' style={{fontWeight:'600', fontSize: '57px', lineHeight: '30px', color: '#4743c9', marginBottom: '24px' }}>100+</p>
-                    <p className='featureText' style={{fontWeight:'400', fontSize: '18px', lineHeight: '30px', color: '#0c266c' }}>Available Countries</p>
+            <div className={`text-center ${styles.featureBox}`}>
+                    <p className={styles.featureIcon} >100+</p>
+                    <p className={styles.featureText}>Available Countries</p>
                 </div>
             </div>
             <div className="col-lg-3 col-6">
-            <div className="text-center">
-                    <p className='featureIcon' style={{fontWeight:'600', fontSize: '57px', lineHeight: '30px', color: '#4743c9', marginBottom: '24px' }}>70+</p>
-                    <p className='featureText' style={{fontWeight:'400', fontSize: '18px', lineHeight: '30px', color: '#0c266c' }}>Payment Methods</p>
+            <div className={`text-center ${styles.featureBox}`}>
+                    <p className={styles.featureIcon} >70+</p>
+                    <p className={styles.featureText} >Payment Methods</p>
                 </div>
             </div>
             <div className="col-lg-3 col-6">
-            <div className="text-center">
-                    <p className='featureIcon' style={{fontWeight:'600', fontSize: '57px', lineHeight: '30px', color: '#4743c9' , marginBottom: '24px'}}>7/24+</p>
-                    <p className='featureText' style={{fontWeight:'400', fontSize: '18px', lineHeight: '30px', color: '#0c266c' }}>Support Team</p>
+            <div className={`text-center ${styles.featureBox}`}>
+                    <p className={styles.featureIcon} >7/24+</p>
+                    <p className={styles.featureText}>Support Team</p>
                 </div>
             </div>
             </div>
